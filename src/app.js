3180bd41fcb91fe772d2b097fa741d7d45d92a58
Dominik Dembski <dembsky09@gmail.com>
2025-01-04 22:02:16 +0100

    Formularze do logowania, rejestracja, logowanie v2 (nie dodalem pliku)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
+// Importowanie bibliotek
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+// Importowanie routerów
 const indexRouter = require('./routes/index.js');
+const signinRouter = require('./routes/signin.js')
+const signupRouter = require('./routes/signup.js')
+const authRouter = require('./routes/auth');
+
+
+// Łączenie z bazą danych
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+
 const app = express();
 
 // Ustawienie EJS
 app.set('view engine', 'ejs');
+
+// Ustawienie pod authAPI
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Ustawienie widoków
 app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +34,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Router
 app.use('/', indexRouter);
+app.use('/signin', signinRouter);
+app.use('/signup', signupRouter);
+app.use('/api/auth', authRouter);
 
 // Uruchomienie serwera
 const PORT = 3000;
